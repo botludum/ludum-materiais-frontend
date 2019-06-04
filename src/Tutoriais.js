5de3330d8745ef82c9d3121de404f77810287007
Wictor Bastos Girardi <wictor.girardi@gmail.com>
2019-06-03 21:27:22 -0300

    #93 - Corrigindo lint

--- src/Tutoriais.js
+++ src/Tutoriais.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
