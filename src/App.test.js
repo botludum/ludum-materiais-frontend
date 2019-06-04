5db94baded21657f0a7d30765f64fdd71328d7df
Wictor Bastos Girardi <wictor.girardi@gmail.com>
2019-06-03 22:13:25 -0300

    #93 - Corrigindo teste

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Components/Tutoriais';
+import Tutoriais from './Components/Tutoriais';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
