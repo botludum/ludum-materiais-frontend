84b6c664852d8c81910cc8ec7e621efb557f6047
Guilherme Siqueira <guilhersiqueira@gmail.com>
2019-06-10 20:44:00 -0300

    Criando navbar diferente para usuário logado e usuário não logado

--- src/helpers/navbar.js
+++ src/helpers/navbar.js
@@ -6,42 +6,74 @@ import AuthService from '../services/AuthService';
 import { withRouter } from 'react-router-dom';
 
 const Auth = new AuthService();
-class  NavBar extends Component {
 
-    handleLogout(){
+class NavBar extends Component {
+
+    handleLogout() {
         Auth.logout()
         this.props.history.replace('/');
-     }
+    }
 
-    handleGerenciarTutorial(){
+    handleGerenciarTutorial() {
         this.props.history.replace('/tutorial/gerenciar');
     }
 
-    handleCadastrarTutorial(){
+    handleCadastrarTutorial() {
         this.props.history.replace('/tutorial/cadastrar');
     }
 
-    handleCadastrarLink(){
+    handleCadastrarLink() {
         this.props.history.replace('/link/cadastrar');
     }
 
-    handleGerenciarLink(){
+    handleGerenciarLink() {
         this.props.history.replace('/link/gerenciar');
     }
-    render() {
-      return (
-        <MuiThemeProvider>
-        <div style={{textAlign : 'center'}}>
-        <AppBar style={{backgroundColor: '#63347f'}} showMenuIconButton={false} >
-            <Button color="inherit" style={{color: "#fff"}} onClick={this.handleCadastrarLink.bind(this)}>Cadastrar Link</Button>
-            <Button color="inherit" style={{color: "#fff"}} onClick={this.handleGerenciarLink.bind(this)}>Gerenciar Links</Button>
-            <Button color="inherit" style={{color: "#fff"}} onClick={this.handleCadastrarTutorial.bind(this)}>Cadastrar Tutoriais</Button>
-            <Button color="inherit" style={{color: "#fff"}} onClick={this.handleGerenciarTutorial.bind(this)}>Gerenciar Tutoriais</Button>
-            <Button color="inherit" style={{color: "#fff"}} onClick={this.handleLogout.bind(this)}>Sair</Button>
-        </AppBar>
-        </div>
-        </MuiThemeProvider>
-      );
+
+    renderNaoAutenticado() {
+        return (
+            <AppBar style={{ backgroundColor: '#63347f' }} showMenuIconButton={false} >
+                <Button color="inherit" style={{ color: "#fff" }} onClick={this.handleCadastrarLink.bind(this)}>Cadastrar Link</Button>
+                <Button color="inherit" style={{ color: "#fff" }} onClick={this.handleCadastrarTutorial.bind(this)}>Cadastrar Tutoriais</Button>
+                <Button color="inherit" style={{ color: "#fff" }} onClick={this.handleLogout.bind(this)}>Entrar</Button>
+            </AppBar>
+        );
     }
-  };
-  export default withRouter(NavBar); 
+
+
+    renderAutenticado() {
+        return (
+            <AppBar style={{ backgroundColor: '#63347f' }} showMenuIconButton={false} >
+                <Button color="inherit" style={{ color: "#fff" }} onClick={this.handleCadastrarLink.bind(this)}>Cadastrar Link</Button>
+                <Button color="inherit" style={{ color: "#fff" }} onClick={this.handleGerenciarLink.bind(this)}>Gerenciar Links</Button>
+                <Button color="inherit" style={{ color: "#fff" }} onClick={this.handleCadastrarTutorial.bind(this)}>Cadastrar Tutoriais</Button>
+                <Button color="inherit" style={{ color: "#fff" }} onClick={this.handleGerenciarTutorial.bind(this)}>Gerenciar Tutoriais</Button>
+                <Button color="inherit" style={{ color: "#fff" }} onClick={this.handleLogout.bind(this)}>Sair</Button>
+            </AppBar>
+        );
+    }
+
+    loadBar(token) {
+        if (token === null) {
+            return
+        }
+    }
+    render() {
+        const token = Auth.getToken();
+        let bar;
+
+        if (token === null) {
+            bar = this.renderNaoAutenticado();
+        } else {
+            bar = this.renderAutenticado();
+        }
+        return (
+            <MuiThemeProvider>
+                <div style={{ textAlign: 'center' }}>
+                    {bar}
+                </div>
+            </MuiThemeProvider>
+        );
+    }
+};
+export default withRouter(NavBar); 
