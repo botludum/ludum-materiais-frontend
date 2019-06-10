41447bfa7d3defbf5dac6b4fff485bd3f787d45f
jprcorreia <joaopedrocorreia9@gmail.com>
2019-06-09 22:34:32 -0300

    #159 - Modificando o metodo de conexão das endpoints
    Co-authored-by: jonjon667 <jonjon667@hotmail.com>

--- src/Tutoriais.js
+++ src/Tutoriais.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import $ from 'jQuery';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
@@ -12,21 +13,18 @@ import IconButton from '@material-ui/core/IconButton';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import DoneIcon from '@material-ui/icons/Done';
 import CloseIcon from '@material-ui/icons/Close';
+import { create } from 'domain';
 
+  var items= [];
+  
 
-  var state={
-    items: [],
-  }
+  const url = ""
 
-  const url = "https://ludum-materiais.herokuapp.com/api/tutoriais"
-
-    fetch(url)
-      .then(res => res.json())
-      .then(json =>{
-        state({
-          items: json,
-        })
-      });
+  var rows = [{nome: null,status: null,autor: null,visualizar: null, aceitar: null, rejeitar: null}]; 
+  
+    function createData(nome, status, autor, visualizar, aceitar, rejeitar) {
+      return { nome, status, autor, visualizar, aceitar, rejeitar };
+    }
   
   const StyledTableCell = withStyles(theme => ({
     head: {
@@ -46,19 +44,6 @@ import CloseIcon from '@material-ui/icons/Close';
     },
   }))(TableRow);
 
-  function createData(nome, status, autor, visualizar, aceitar, rejeitar) {
-    return { nome, status, autor, visualizar, aceitar, rejeitar };
-  }
-
-
-
-  const rows = [
-    state.items(item => (
-      createData(item.title,item.status,item._id)
-    ))
-  ];
-
-
 
   const useStyles = makeStyles(theme => ({
     root: {
@@ -75,66 +60,84 @@ import CloseIcon from '@material-ui/icons/Close';
     },
   }));
 
-  function Tutoriais() {
-    const classes = useStyles();
-    return (
-      <div> 
-        <MuiThemeProvider>
-          <div style={style}>
-            <AppBar
-              style={{backgroundColor: '#63347f'}}
-              title="Tutoriais"
-             />
-            <br/>
-          </div>
-      </MuiThemeProvider>
-      <br/>
-      <br/>
-        <Paper className={classes.root}>
-          <Table className={classes.table}>
-            <TableHead>
-              <TableRow>
-                <StyledTableCell align="left">TITULO DO TUTORIAL</StyledTableCell>
-                <StyledTableCell align="right">STATUS</StyledTableCell>
-                <StyledTableCell align="right">AUTOR</StyledTableCell>
-                <StyledTableCell align="right">VISUALIZAR</StyledTableCell>
-                <StyledTableCell align="right">APROVAR</StyledTableCell>
-                <StyledTableCell align="right">REJEITAR</StyledTableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map(row => (
-                <StyledTableRow key={row.nome}>
-                  <StyledTableCell component="th" scope="row">
-                    {row.nome}
-                  </StyledTableCell>
-                  <StyledTableCell align="right">{row.status}</StyledTableCell>
-                  <StyledTableCell align="right">{row.autor}</StyledTableCell>
-                  <StyledTableCell align="right">{row.visualizar}
-                    <IconButton className={classes.button} aria-label="Visualizar">
-                      <VisibilityIcon />
-                    </IconButton>
-                  </StyledTableCell>
-                  <StyledTableCell align="right">{row.aceitar}
-                    <IconButton className={classes.button} aria-label="Aceitar">
-                      <DoneIcon />
-                    </IconButton>
-                  </StyledTableCell>
-                  <StyledTableCell align="right">{row.rejeitar}
-                    <IconButton className={classes.button} aria-label="Rejeitar">
-                      <CloseIcon />
-                    </IconButton>
-                  </StyledTableCell>
-                </StyledTableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </Paper>
-      </div>
-    )
-  }
   
+  function buscaEP(url){
+    console.log("Busca");
+    const classes = useStyles;
+    var result = [];
+    fetch('https://ludum-materiais.herokuapp.com/api/tutoriais/',{
+      method: "GET"
+    })
+          .then((res) => {
+            res.json().then((json) => {
+              console.log(json);
+              json.data.forEach(element => {
+                console.log(element.title)
+                rows.push(createData(element.title,element.status,element._id))
+              });
+              console.log("row");
+              console.log(rows)
+              rows.push(createData("4","2","3"));
+              return (
+                <div> 
+                  <MuiThemeProvider>
+                    <div style={style}>
+                      <AppBar
+                        style={{backgroundColor: '#63347f'}}
+                        title="Tutoriais"
+                       />
+                      <br/>
+                    </div>
+                </MuiThemeProvider>
+                <br/>
+                <br/>
+                  <Paper className={classes.root}>
+                    <Table className={classes.table}>
+                      <TableHead>
+                        <TableRow>
+                          <StyledTableCell align="left">TITULO DO TUTORIAL</StyledTableCell>
+                          <StyledTableCell align="right">STATUS</StyledTableCell>
+                          <StyledTableCell align="right">AUTOR</StyledTableCell>
+                          <StyledTableCell align="right">VISUALIZAR</StyledTableCell>
+                          <StyledTableCell align="right">APROVAR</StyledTableCell>
+                          <StyledTableCell align="right">REJEITAR</StyledTableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        {rows.map(row => (
+                          <StyledTableRow key={row.autor}>
+                            <StyledTableCell component="th" scope="row">
+                              {row.nome}
+                            </StyledTableCell>
+                            <StyledTableCell align="right">{row.status}</StyledTableCell>
+                            <StyledTableCell align="right">{row.autor}</StyledTableCell>
+                            <StyledTableCell align="right">{row.visualizar}
+                              <IconButton className={classes.button} aria-label="Visualizar">
+                                <VisibilityIcon />
+                              </IconButton>
+                            </StyledTableCell>
+                            <StyledTableCell align="right">{row.aceitar}
+                              <IconButton className={classes.button} aria-label="Aceitar">
+                                <DoneIcon />
+                              </IconButton>
+                            </StyledTableCell>
+                            <StyledTableCell align="right">{row.rejeitar}
+                              <IconButton className={classes.button} aria-label="Rejeitar">
+                                <CloseIcon />
+                              </IconButton>
+                            </StyledTableCell>
+                          </StyledTableRow>
+                        ))}
+                      </TableBody>
+                    </Table>
+                  </Paper>
+                </div>
+              )
+            })
+          })
+
+    };
   const style = {
     textAlign : 'center',
   };
-  export default Tutoriais;
+  export default buscaEP;
